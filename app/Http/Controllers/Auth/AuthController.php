3b9a3a53e6bcb18cfe5ba585cb937a89d73c1a7b
sudip2222 <sudipdiu@gmail.com>
2016-11-28 10:27:35 +0600

    AuthController Update

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -11,12 +11,41 @@ class AuthController extends Controller
 {
 
     public function redirectToProvider(){
+
         return Socialite::driver('github')->redirect();
+
+
     }
 
     public function handleProviderCallback(){
 
-        $githubUser=Socialite::driver('github')->user();
+        /*$githubUser=Socialite::driver('github')->user();
+
+        dd($githubUser);*/
+        $user = $this->findOrCreateGitHubUser(
+
+            Socialite::driver('github')->user()
+
+        );
+
+        auth()->login($user);
+
+        return redirect('/');
+    }
+
+    protected function findOrCreateGitHubUser($githubUser){
+
+        $user = User::firstOrNew(['github_id' => $githubUser->id]);
+
+        if ($user->exists) return $user;
+
+        $user->fill([
+            'username' => $githubUser->nickname,
+            'email' => $githubUser->email,
+            'avatar' => $githubUser->avatar
+        ])->save();
+
+        return $user;
     }
 
 }
